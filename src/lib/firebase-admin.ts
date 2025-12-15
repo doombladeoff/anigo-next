9aaa43c99e84c50460138a927647bca6c0a8a6a0
Zhovtonizhko Dmitriy <47075934+doombladeoff@users.noreply.github.com>
2025-12-15 17:18:42 +0200

    Update firebase-admin.ts

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,20 @@
-import * as admin from "firebase-admin";
+import admin from "firebase-admin";
+
+function formatPrivateKey(key?: string) {
+    if (!key) return undefined;
+    return key.replace(/\\n/g, "\n");
+}
+
+const privateKey = formatPrivateKey(process.env.FIREBASE_PRIVATE_KEY);
 
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID as string,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY as string,
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey,
         }),
     });
 }
+
 export default admin;
