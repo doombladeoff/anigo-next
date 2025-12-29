7b6b03fe09a454bddf8e117929afb6676550ab4f
Zhovtonizhko Dmitriy <47075934+doombladeoff@users.noreply.github.com>
2025-12-29 20:06:37 +0200

    Update LastUpdateItem.tsx

--- src/components/pages/home/LastUpdatesAnime/LastUpdateItem.tsx
+++ src/components/pages/home/LastUpdatesAnime/LastUpdateItem.tsx
@@ -1,29 +1,42 @@
 import Image from "next/image";
 import { KodikMaterialObject } from "@/app/types/Kodik.types";
+import Link from "next/link";
+import slugify from "slugify";
 
 export const LastUpdateItem = ({ item, index }: { item: KodikMaterialObject; index: number }) => {
     const poster = item?.material_data?.anime_poster_url || "";
     const title = item?.title || "";
     const translate = item?.translation?.title || "";
-    const isVisible = index < 10;
+    const isLCP = index < 6;
 
     return (
-        <>
+        <Link
+            key={item.id}
+            href={`/anime/${slugify(item.material_data.title_en!,
+                {
+                    replacement: '-',
+                    remove: undefined,
+                    lower: true,
+                    strict: true,
+                    trim: true
+                })}-${item.shikimori_id}`
+            }
+            className="w-[180px] shrink-0 cursor-pointer group"
+            draggable={false}
+        >
             <div className={"w-[180px] shrink-0 cursor-pointer group transition-transform duration-300 ease-out transform hover:-translate-y-1"}>
                 <div className="relative w-full h-[260px] overflow-hidden rounded-xl shadow-2xl bg-black/25 dark:bg-white/15 opacity-0 animate-fade">
                     <Image
-                        src={poster || ""}
+                        src={poster}
                         alt={title}
-                        unoptimized
                         fill
-                        className="object-cover"
-                        sizes="180px"
+                        sizes="(max-width: 640px) 140px, (max-width: 1024px) 160px, 180px"
+                        quality={25}
+                        className="object-cover bg-black/25 dark:bg-white/15 h-[250px] w-auto"
+                        loading={isLCP ? "eager" : "lazy"}
+                        fetchPriority={isLCP ? "high" : undefined}
                         draggable={false}
-                        loading={isVisible ? "eager" : "lazy"}
-                        priority={isVisible}
-                        quality={75}
                     />
-
                     <div className="absolute bottom-0 w-full h-10 flex items-center justify-center">
                         <div className="absolute inset-0 bg-linear-to-t from-black to-transparent rounded-b-xl" />
                         <span className="relative text-white font-semibold text-sm">
@@ -39,6 +52,6 @@ export const LastUpdateItem = ({ item, index }: { item: KodikMaterialObject; ind
             <p className="text-xs opacity-60 mt-1 line-clamp-1">
                 {translate}
             </p>
-        </>
+        </Link>
     );
 };
