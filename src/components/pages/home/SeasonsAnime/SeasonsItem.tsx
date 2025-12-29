01461e6ccbe7890806742ca3a1a7f47c9800a641
Zhovtonizhko Dmitriy <47075934+doombladeoff@users.noreply.github.com>
2025-12-29 20:06:26 +0200

    Update SeasonsItem.tsx

--- src/components/pages/home/SeasonsAnime/SeasonsItem.tsx
+++ src/components/pages/home/SeasonsAnime/SeasonsItem.tsx
@@ -1,52 +1,50 @@
 import Image from "next/image";
 import { ShikimoriAnime } from "@/app/types/Shikimori.types";
+import Link from "next/link";
+import slugify from "slugify";
 
-export const SeasonItem = ({ item }: { item: ShikimoriAnime; }) => {
+export const SeasonItem = ({ item, index }: { item: ShikimoriAnime; index: number }) => {
+
+    const isLCP = index < 3;
+
+    const poster = item.poster.preview2xUrl;
+    const score = item.score.toFixed(1);
+
     return (
-        <div className="w-[180px] shrink-0 group">
-            <div className="relative w-[180px] h-[260px] rounded-xl overflow-hidden shadow-2xl">
-                <Image
-                    src={item.poster.mainUrl}
-                    alt={item.russian || item.name}
-                    fill
-                    sizes="180px"
-                    quality={50}
-                    loading={'eager'}
-                    fetchPriority='high'
-                    draggable={false}
-                    className="
-                        object-cover
-                        bg-black/25
-                        dark:bg-white/15
-                        transition-transform duration-500 ease-out
-                        group-hover:scale-105
-                    "
-                />
+        <Link
+            draggable={false}
+            href={`/anime/${slugify(item.name, {
+                replacement: '-',
+                remove: undefined,
+                lower: true,
+                strict: true,
+                trim: true
+            })}-${item.id}`}
+        >
+            <div className="group w-[180px] py-2">
+                <div className="relative w-[180px] h-[250px] rounded-xl overflow-hidden shadow-2xl transition-transform duration-300 ease-out transform hover:-translate-y-1">
+                    <Image
+                        src={poster}
+                        alt={item.russian || item.name}
+                        fill
+                        sizes="(max-width: 640px) 140px, (max-width: 1024px) 160px, 180px"
+                        quality={30}
+                        className="object-cover bg-black/25 dark:bg-white/15"
+                        loading={isLCP ? "eager" : "lazy"}
+                        fetchPriority={isLCP ? "high" : "auto"}
+                        draggable={false}
+                    />
 
-                {item.score && (
-                    <div className="absolute top-2 left-2 px-2 py-1 rounded-full bg-green-500 text-white text-xs font-bold shadow-lg">
-                        {item.score.toFixed(1)}
-                    </div>
-                )}
-
-                <div className="
-                    absolute inset-0
-                    bg-black/60
-                    opacity-0
-                    transition-opacity duration-300
-                    group-hover:opacity-100
-                    flex items-center justify-center
-                    px-2 text-center
-                ">
-                    <span className="text-white text-sm font-semibold line-clamp-3">
-                        {item.russian}
-                    </span>
+                    {item.score && (
+                        <div className="absolute top-2 left-2 px-2 py-1 rounded-full bg-green-700 text-white dark:bg-green-400 dark:text-black text-xs font-bold shadow-lg">
+                            {score}
+                        </div>
+                    )}
                 </div>
+                <p className="mt-2 text-sm font-semibold line-clamp-2 opacity-90">
+                    {item.russian}
+                </p>
             </div>
-
-            <p className="mt-2 text-sm font-semibold line-clamp-2 opacity-90">
-                {item.russian}
-            </p>
-        </div>
+        </Link>
     );
 };
